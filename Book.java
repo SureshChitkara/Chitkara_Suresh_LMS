70bc9395cd7a9d739fe6a98ea9b83f25c6b7ed6e
SureshChitkara <156342133+SureshChitkara@users.noreply.github.com>
2024-01-20 17:55:26 -0500

    Book.java

--- Book.java
+++ Book.java
@@ -1,3 +1,9 @@
+/****************************************SC***************************************************
+ * Name: Suresh Chitkara *
+ * Course: Software Development I CEN-3024C-24667 *
+ * Date: 1/20/2024 *
+ * Description: The Book Class creates objects which are ID, title, and author. It allows user to enter an id, title and author. 
+ *****************************************SC****************************************************/
 public class Book {
     private int id;
     private String title;
