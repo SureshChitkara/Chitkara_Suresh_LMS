0524a013faf7b10c7e1ce5b366e0bbd65a130a3c
SureshChitkara <156342133+SureshChitkara@users.noreply.github.com>
2024-01-23 11:46:42 -0500

    main.java

--- main.java
+++ main.java
@@ -1,6 +1,12 @@
-public class main {
-    public static void main(String[] args) {
-        LibraryManagementSystem lms = new LibraryManagementSystem();
+/****************************************SC***************************************************
+ * Name: Suresh Chitkara *
+ * Course: Software Development I CEN-3024C-24667 *
+ * Date: 1/23/2024 *
+ * Description: The main class is the entry point of a LibraryManagementSystem
+ *****************************************SC****************************************************/
+public class main { // SC Main class begins.
+    public static void main(String[] args) { // SC The main method creates an instance of LibraryManagementSystem, adds books from a file, and interacts with users through a simple console menu. 
+        LibraryManagementSystem lms = new LibraryManagementSystem(); // SC Creates a new instance of the LibraryManagementSystem class.
         lms.addBooksFromFile("books.txt");
         lms.listAllBooks();
         lms.removeBookById(2);
