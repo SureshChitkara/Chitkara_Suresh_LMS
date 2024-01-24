56965a2e8356a141efdb656d17aa09e8eb041a0c
SureshChitkara <156342133+SureshChitkara@users.noreply.github.com>
2024-01-24 17:39:34 -0500

    main.java

--- main.java
+++ main.java
@@ -7,7 +7,7 @@
 public class main { /* SC Main class begins. */
     public static void main(String[] args) { /* SC The main method creates an instance of LibraryManagementSystem, adds books from a file, and interacts with users through a simple console menu. */ 
         LibraryManagementSystem lms = new LibraryManagementSystem(); /* SC Creates a new instance of the LibraryManagementSystem class. */
-        lms.addBooksFromFile("books.txt"); /* SC The method attempts to read books from the specified file path. */
+        lms.addBooksFromFile("books.txt"); /* SC The method attempts to read books from the file books.txt. */
         lms.listAllBooks(); /* SC The method displays a list of all books currently in the library's collection. */
         lms.removeBookById(2); /* SC Method removes book from the collection depending on the ID which is 2. */
         lms.listAllBooks(); /* SC Method lists the books after the removeBookById has been done. */
